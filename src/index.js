863bb41621d44f0f1582295777346196444e9965
krunal vatvani <krunalvatvani2424@gmail.com>
2023-11-25 15:19:30 +0530

    first deployment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Nav from './components/Nav';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/">
+
+    {/* <App /> */}
+    <Nav />
+    </BrowserRouter>,
+
   </React.StrictMode>
 );
 

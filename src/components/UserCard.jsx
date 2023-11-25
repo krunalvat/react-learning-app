7a036077196c847d732c471bd393702f51f2328d
krunal vatvani <krunalvatvani2424@gmail.com>
2023-11-25 23:32:48 +0530

    first deployment

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -5,7 +5,7 @@ return (
 <>
 
     <div className="card-group" id ={ props.key }style= { {width: "18rem"}}>
-        <img className="card-img-top" src= { props.image}  />
+        <img className="card-img-top" src= { props.image} alt="user-image" />
         <div className="card-body">
             <h5 className="card-title">{ props.name }</h5>
             <p className="card-text"> { props.email}</p>

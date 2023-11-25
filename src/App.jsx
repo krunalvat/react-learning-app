b4a3d803cf88636218b9c1852e3b371c92eeaf32
krunal vatvani <krunalvatvani2424@gmail.com>
2023-11-25 13:20:08 +0530

    first deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+// import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 import './App.css';
 
-import UseContext from './components/UseContext';
-import UserPrams from './components/UserPrams';
+// import UseContext from './components/UseContext';
+// import UserPrams from './components/UserPrams';
 
-import Nav from './components/Nav';
-import Name from "./components/Name";
-/** Hard Coded */
-import Form from './components/Form';
-/** Little Hard Coded */
-import FormSimple from './components/FormSimple';
-import FormSimpleTwo from './components/FormSimpleTwo';
-import User from './components/User';
-import UserCard from './components/UserCard';
-import Props from './components/Props';
-import ToDoListToggle from './components/ToDoListToggle';
-import ToDoList1 from './components/ToDoList1';
-import UseStateExample from './components/UseStateExample';
-import ImageGallery from './components/ImageGallery';
-import Challenge from "./components/Challenge";
-import ChallengeOne from './components/ChallengeOne';
-import ChallengeTwo from './components/ChallengeTwo';
-import ChallengeThree from './components/ChallengeThree';
-import Search from './components/Search';
+// import Nav from './components/Nav';
+// import Name from "./components/Name";
+// /** Hard Coded */
+// import Form from './components/Form';
+// /** Little Hard Coded */
+// import FormSimple from './components/FormSimple';
+// import FormSimpleTwo from './components/FormSimpleTwo';
+// import User from './components/User';
+// import UserCard from './components/UserCard';
+// import Props from './components/Props';
+// import ToDoListToggle from './components/ToDoListToggle';
+// import ToDoList1 from './components/ToDoList1';
+// import UseStateExample from './components/UseStateExample';
+// import ImageGallery from './components/ImageGallery';
+// import Challenge from "./components/Challenge";
+// import ChallengeOne from './components/ChallengeOne';
+// import ChallengeTwo from './components/ChallengeTwo';
+// import ChallengeThree from './components/ChallengeThree';
+// import Search from './components/Search';
 
 function App() {
   return (
